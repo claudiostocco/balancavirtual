2325b06b269680c5d950eb62a876c3c7a4fa9b00
claudiostocco <claudiostocco@gmail.com>
2024-06-13 16:56:19 -0300

    Initial commit

---
+++ main.pas
@@ -0,0 +1,26 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfmMain = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  fmMain: TfmMain;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
